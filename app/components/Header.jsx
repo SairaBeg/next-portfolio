bd52faa99dead999b22c325eb547ed9323174072
SairaBeg <Sairabeg88@gmail.com>
2023-10-29 21:38:03 -0400

    Fixed file structure

---
+++ app/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <section>
+      <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+        Hello! I'm Saira
+      </h1>
+      <p className="text-[#ADB&BE]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+        commodi, ratione animi debitis, recusandae, veritatis officiis dolores
+        placeat facere quaerat fugit rerum non nam qui architecto nostrum dicta
+        corporis! Incidunt!
+      </p>
+    </section>
+  );
+};
+export default Header;
---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import Header from "./components/Header";
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
+      <Header />
+    </main>
+  );
+}
