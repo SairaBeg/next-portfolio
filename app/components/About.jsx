44170915da66025f4139f9c9d8d8d7254a083772
SairaBeg <Sairabeg88@gmail.com>
2023-10-31 21:56:57 -0400

    About section tabs

---
+++ app/components/About.jsx
@@ -0,0 +1,74 @@
+"use client";
+import React, { useState, useTransition } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul>
+        <li>Next.js</li>
+        <li>React</li>
+        <li>HTML5</li>
+        <li>CSS & TailwindCSS</li>
+        <li>JavaScript</li>
+        <li>Node.js</li>
+        <li>Express.js</li>
+      </ul>
+    ),
+  },
+];
+
+const About = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px16">
+        <Image src="/images/VapoDesk.png" alt="desk" height={500} width={500} />
+        <div>
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            iste officiis recusandae iure laudantium accusamus provident culpa
+            aperiam dolorum? Reprehenderit ipsam eligendi temporibus
+            perspiciatis, autem commodi sed harum culpa pariatur.
+          </p>
+          <div className="flex flex-row mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("experience")}
+              active={tab === "experience"}
+            >
+              {" "}
+              Experience{" "}
+            </TabButton>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <div className="container mx-auto px-12 py-4 mt-24">
         <Hero />
+        <About />
       </div>
     </main>
   );
