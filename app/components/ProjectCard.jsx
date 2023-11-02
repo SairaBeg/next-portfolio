dd9d0c2ccf9a046369abb89ba907055cef93eee0
SairaBeg <Sairabeg88@gmail.com>
2023-11-02 17:04:43 -0400

    Project section complete

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -7,7 +7,11 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
     <div>
       <div
         className="h-52 md:h-72  rounded-t-xl relative group"
-        style={{ background: `url(${imgUrl})`, backgroundSize: "100% auto" }}
+        style={{
+          background: `url(${imgUrl})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
@@ -24,7 +28,7 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
           </Link>
         </div>
       </div>
-      <div className="text-white rounded-b-xl bg[#181818] py-6 px-4">
+      <div className="text-white mt-3 rounded-b-xl bg[#181818] py-6 px-4">
         <h5 className="text-xl font-semibold mb-2">{title}</h5>
         <p className="text-[#ADB7BE]">{description}</p>
       </div>
--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -8,6 +8,8 @@ const projectData = [
     description: "Project 1 desc",
     image: "/projectPics/AoWscreenshot.png",
     tag: ["All", "Web"],
+    gitUrl: "https://github.com/SairaBeg/AngelsOnWheelsWebsite",
+    previewUrl: "https://jenniferp98.sg-host.com/AngelsOnWheels/index.php",
   },
   {
     id: 2,
@@ -15,6 +17,8 @@ const projectData = [
     description: "Project 2 desc",
     image: "/projectPics/AI-Prompts.png",
     tag: ["All", "Web"],
+    gitUrl: "https://github.com/SairaBeg/ai-prompts",
+    previewUrl: "https://ai-prompts-r4coab5ym-sairabeg.vercel.app/",
   },
   {
     id: 3,
@@ -22,6 +26,8 @@ const projectData = [
     description: "Project 3 desc",
     image: "/projectPics/EcommerceWS.png",
     tag: ["All", "Web"],
+    gitUrl: "https://github.com/SairaBeg/EcommerceWebsite",
+    previewUrl: "https://e-commercewebsite.pages.dev/",
   },
   {
     id: 4,
@@ -29,6 +35,8 @@ const projectData = [
     description: "Project 4 desc",
     image: "/projectPics/greyscaleCropped.png",
     tag: ["All", "Web"],
+    gitUrl: "https://github.com/SairaBeg/GreyscaleConverter",
+    previewUrl: "/",
   },
   {
     id: 5,
@@ -36,6 +44,8 @@ const projectData = [
     description: "Project 5 desc",
     image: "/projectPics/spelling.jpg",
     tag: ["All", "Web"],
+    gitUrl: "https://github.com/SairaBeg/Spell-Checker",
+    previewUrl: "/",
   },
   {
     id: 6,
@@ -43,20 +53,26 @@ const projectData = [
     description: "Project 6 desc",
     image: "/projectPics/sentiment.png",
     tag: ["All", "Web"],
+    gitUrl: "https://github.com/SairaBeg/SentimentAnalyzer",
+    previewUrl: "/",
   },
 ];
 
 const Projects = () => {
   return (
     <>
-      <h2>My Projects</h2>
-      <div>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
         {projectData.map((project) => (
           <ProjectCard
             key={project.id}
             title={project.title}
             description={project.description}
             imgUrl={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
           />
         ))}
       </div>
